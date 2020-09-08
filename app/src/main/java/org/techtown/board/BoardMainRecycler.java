877fa8a265c2f608690fecff12ce381aad3543e6
kangjisoo <z100444@hanmail.net>
2020-09-08 16:31:37 +0900

    2020-09-08 강지수 -BoardAdd생성

--- app/src/main/java/org/techtown/board/BoardMainRecycler.java
+++ app/src/main/java/org/techtown/board/BoardMainRecycler.java
@@ -1,23 +1,30 @@
 package org.techtown.board;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import org.techtown.loginactivity.R;
+import org.techtown.projectmain.ProjectAdd;
+import org.techtown.projectmain.ProjectHome;
 
 
 public class BoardMainRecycler extends AppCompatActivity {
     RecyclerView recyclerView;
+    ImageButton imageButton;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //툴바의 옵션메뉴 동작
+//        툴바의 옵션메뉴 동작
 //        setHasOptionsMenu(true);
         setContentView(R.layout.board_main);
         BoardAdapter adapter = new BoardAdapter(this);
@@ -30,7 +37,15 @@ public class BoardMainRecycler extends AppCompatActivity {
         adapter.addItem(new BoardList("안녕하세용?","지수!!!","2020-09-07"));
         recyclerView.setAdapter(adapter);
 
-//        return rootView;
+        //플러스 버튼 누르면 프로젝트생성 액티비티로 전환
+        imageButton = (ImageButton)findViewById(R.id.post_add);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(BoardMainRecycler.this, BoardAdd.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
