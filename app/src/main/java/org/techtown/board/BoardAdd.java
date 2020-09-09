31113b0575fea85ff6c856ae88a1e0289cacc26f
kangjisoo <z100444@hanmail.net>
2020-09-09 16:25:51 +0900

    2020-09-09 강지수 -board_add.xml  -게시물 작성 공간에 ScrollView삽입  -ScrollView안에 EditText추가하여 글 작성할 수 있도록 구현  -사진 버튼 눌렀을 때 갤러리 연결시킴

--- app/src/main/java/org/techtown/board/BoardAdd.java
+++ app/src/main/java/org/techtown/board/BoardAdd.java
@@ -1,21 +1,139 @@
 package org.techtown.board;
 
+import android.Manifest;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.text.method.ScrollingMovementMethod;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
 
 import org.techtown.loginactivity.R;
 import org.techtown.projectmain.ProjectAdd;
 
+import java.io.File;
+import java.util.ArrayList;
+
 public class BoardAdd extends AppCompatActivity {
-    ImageButton imageButton;
+    private ScrollView board_scrollView;
+    private EditText board_text;
+
+    private static final int PICK_FROM_ALBUM = 1;
+    private File tempFile;
+    Button image;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.board_add);
+        tedPermission();
+        //스크롤뷰와 스크롤뷰 안의 텍스트 객체화
+        board_scrollView = (ScrollView) findViewById(R.id.board_scrollView);
+        board_text = (EditText) findViewById(R.id.board_textView);
+        //텍스트에 스크롤 선언
+        board_text.setMovementMethod(new ScrollingMovementMethod());
+        board_text.setFocusableInTouchMode(true);
+        board_text.setFocusable(true);
+        board_text.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                //스크롤뷰가 텍스트뷰의 터치이벤트를 가져가지 못하게 함
+                board_scrollView.requestDisallowInterceptTouchEvent(true);
+                return false;
+            }
+        });
 
+        image = (Button) findViewById(R.id.board_add_picture);
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                startActivityForResult(intent, PICK_FROM_ALBUM);
+            }
+        });
+    }//onCreate() 끝
+
+    private void tedPermission() {
+        PermissionListener permissionListener = new PermissionListener() {
+            @Override
+            public void onPermissionGranted() {
+                // 권한 요청 성공
+
+            }
+
+            @Override
+            public void onPermissionDenied(ArrayList<String> deniedPermissions) {
+                // 권한 요청 실패
+            }
+        };
+
+        TedPermission.with(this)
+                .setPermissionListener(permissionListener)
+                .setPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA)
+                .check();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == PICK_FROM_ALBUM) {
+
+            Uri photoUri = data.getData();
+
+            Cursor cursor = null;
+
+            try {
+
+                /*
+                 *  Uri 스키마를
+                 *  content:/// 에서 file:/// 로  변경한다.
+                 */
+                String[] proj = { MediaStore.Images.Media.DATA };
+
+                assert photoUri != null;
+                cursor = getContentResolver().query(photoUri, proj, null, null, null);
+
+                assert cursor != null;
+                int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+
+                cursor.moveToFirst();
+
+                tempFile = new File(cursor.getString(column_index));
+
+            } finally {
+                if (cursor != null) {
+                    cursor.close();
+                }
+            }
+
+            setImage();
+
+        }
+    }
+
+    private void setImage() {
+
+        ImageView imageView = findViewById(R.id.board_imageView);
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        Bitmap originalBm = BitmapFactory.decodeFile(tempFile.getAbsolutePath(), options);
+
+        imageView.setImageBitmap(originalBm);
 
     }
 }
