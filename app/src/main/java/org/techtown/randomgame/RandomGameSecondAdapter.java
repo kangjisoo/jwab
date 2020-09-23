7e298881e079ce1c50c37300e23d612e961a04bf
kimdoa0724 <aod1409@naver.com>
2020-09-23 14:54:16 +0900

    2020.09.23(수)_김도아     <랜덤 뽑기>     - java       1) RandomGameData       2) RandomGameFirst       3) RandomGameAdapter       4) RandomGameSecond       5) RandomGameSecondAdapter       = 첫번째 화면 실행: 뽑기 제목 설정 텍스트뷰, 인원 추가 삭제 버튼 구성, 리사이클러뷰생성, 리사이클러뷰 아이템 추가 버튼과 추가시 아이템 생성, 확인버튼 생성       = 확인버튼 클릭시 뽑기 제목이 공백인지 확인, 인원수와 뽑기 목록수가 같은지 확인 , 목록에 빈값이 없는지 확인후 모든 조건 만족하면 두번째 화면으로 이동, 두번쨰 화면에 리사이클러뷰 뽑기 목록값 전달, 첫번째 리사이클러뷰에 처음에는 비어있는 값을 넣고 text수정이 완료되면 완료된값이 data에 들어감       =두번째 화면 : 제목과 리사이클러뷰에 아이템들을 받아와 리사이클러뷰에 등록 버튼은 가리게 역할, 클릭시 INViSIBLE되어 결과를 확인할 수 있도록 구성, 다시섞기 버튼 클릭시 아이템들 다시 섞여서 출력

--- app/src/main/java/org/techtown/randomgame/RandomGameSecondAdapter.java
+++ app/src/main/java/org/techtown/randomgame/RandomGameSecondAdapter.java
@@ -12,9 +12,12 @@ import androidx.recyclerview.widget.RecyclerView;
 import org.techtown.loginactivity.R;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class RandomGameSecondAdapter extends RecyclerView.Adapter<RandomGameSecondAdapter.ViewHolder> {
     private ArrayList<RandomGameData> sRandomLists;
+    private int count;
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
@@ -45,6 +48,7 @@ public class RandomGameSecondAdapter extends RecyclerView.Adapter<RandomGameSeco
     }
 
     public void onBindViewHolder(RandomGameSecondAdapter.ViewHolder viewHolder, int position) {
+
         RandomGameData randomGameData = sRandomLists.get(position);
 
         viewHolder.countNum.setGravity(Gravity.CENTER);
@@ -53,20 +57,27 @@ public class RandomGameSecondAdapter extends RecyclerView.Adapter<RandomGameSeco
         viewHolder.countNum.setTextSize(TypedValue.COMPLEX_UNIT_SP, 15);
         viewHolder.resultListView2.setTextSize(TypedValue.COMPLEX_UNIT_SP, 15);
 
-        //순서를 줄 것
-        int count=1;
+            //순서를 줄 것
+            count=sRandomLists.get(0).getInitNum()+1;
+            sRandomLists.get(0).setInitNum(count);
             viewHolder.countNum.setText(Integer.toString(count));
-        count=+1;
+
+
         for (int i =0; i<=RandomGameFirst.getnumOfMember();i++){
 
             viewHolder.resultListView2.setText(randomGameData.getRandomGameListItem());
         }
-
     }
+
 
     @Override
     public int getItemCount()  {
         return (null != sRandomLists ? sRandomLists.size() : 0);
     }
+
+    public int nnum(){
+        return 0;
+    }
+
 }
 
