c4965b1d59323942c7efcfb9a5768d3f088f421f
kimdoa0724 <aod1409@naver.com>
2020-10-05 13:27:37 +0900

    2020.09.28(월)_김도아 - 투표기능 -  *java    - VoteAdapter    - VoteData    - VoteMain
       ->투표 메인화면 구성
          투표 제목 입력
          투표 항목을 입력하고 추가 버튼 클릭 시 리스트에 아이템으로 추가
          체크 박스를 선택하고 삭제 버튼 클릭시 아이템 항목에서 삭제
          확인 클리시 db에 조회하여 투표 테이블 생성
          + 추가 버튼 클릭시 아이템 리사이클러뷰에 올리기
          + 체크박스 선택 후 삭제 클릭시 목록에서 삭제
          + 전체선택 클릭시 목록들 전체선택, 해제 기능
          + 리스트 갯수를 보여주는 TextView (추가, 삭제시 증가 감소)
          -> 구현 완료
          -------확인버튼 클릭시 db에서 테이블 생성 만드는 쓰레드 생성필요
      * xml
        vote_main_dp.xml
        vote_item.xml
    
        -> 메인 화면과 리사이클러뷰에 추가될 아이템 카드뷰 작성

--- app/src/main/java/org/techtown/vote/VoteMain.java
+++ app/src/main/java/org/techtown/vote/VoteMain.java
@@ -1,9 +1,11 @@
 package org.techtown.vote;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,6 +15,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import org.techtown.loginactivity.R;
+import org.techtown.projectmain.ProjectAdd;
 
 import java.util.ArrayList;
 
@@ -49,12 +52,35 @@ public class VoteMain extends AppCompatActivity {
                 new LinearLayoutManager(this);
 
         voteRecyclerView.setLayoutManager(layoutManager);
+        vArrayList= new ArrayList<>();
 
         //list갯수 = 0
         listCountTextView.setText(Integer.toString(listNum));
 
         vAdapter = new VoteAdapter(vArrayList);
         voteRecyclerView.setAdapter(vAdapter);
+
+            voteAllCheckBox.setChecked(false);
+
+        voteAllCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (voteAllCheckBox.isChecked()==true){
+                    for (int i =0; i< vArrayList.size(); i++){
+                        VoteData allCheckList = vArrayList.get(i);
+                        allCheckList.setCheckBoxCh(true);
+                    }
+                }
+                else
+                {
+                    for (int i =0; i<vArrayList.size(); i++){
+                        VoteData allCheckList = vArrayList.get(i);
+                        allCheckList.setCheckBoxCh(false);
+                    }
+                }
+                vAdapter.notifyDataSetChanged();
+            }
+        });
 
         //추가버튼 클릭
         voteAddBt.setOnClickListener(new View.OnClickListener() {
@@ -66,16 +92,33 @@ public class VoteMain extends AppCompatActivity {
                 //추가할 항목이 공백이거나 비워져 있을 때
                 if (getList.equals("") || getList.trim().equals("")){
                     Toast.makeText(VoteMain.this,"투표 항목을 입력해주세요",Toast.LENGTH_LONG).show();
+
                 }
+
                 else{
 
-                    //리스트 추가
-                    VoteData newVoteData = new VoteData(false, getList);
-                    vArrayList.add(newVoteData);
-                    vAdapter.notifyItemInserted(listNum);
-                    listNum++;
-                    listCountTextView.setText(Integer.toString(listNum));
+                    if (voteAllCheckBox.isChecked()==true){
+                        //리스트 추가
+                        VoteData newVoteData = new VoteData(true, getList);
+                        vArrayList.add(newVoteData);
+                        vAdapter.notifyItemInserted(vAdapter.getItemCount());
 
+                        listNum++;
+                        listCountTextView.setText(Integer.toString(listNum));
+                    }
+                    else {
+                        //리스트 추가
+                        VoteData newVoteData = new VoteData(false, getList);
+                        vArrayList.add(newVoteData);
+                        vAdapter.notifyItemInserted(vAdapter.getItemCount());
+
+                        listNum++;
+                        listCountTextView.setText(Integer.toString(listNum));
+
+                    }
+
+                    //입력창 비워주기
+                    voteAddList.getText().clear();
                 }
             }
         });
@@ -89,6 +132,39 @@ public class VoteMain extends AppCompatActivity {
                 int listSizeCheck = vArrayList.size();
                 int deleteListSize = vArrayList.size();
 
+                for(int j=0; j<=listSizeCheck; j++){
+                    if (vArrayList.size()==0){
+                        deleteListSize = vArrayList.size();
+                        break;
+                    }
+
+                    else{
+                        for (int i=0; i<vArrayList.size(); i++){
+
+                            VoteData voteCheck = vArrayList.get(i);
+
+                            if (voteCheck.isCheckBoxCh()){
+
+                                vArrayList.remove(i);
+
+                                deleteListSize = vArrayList.size();
+
+                                listNum--;
+                                listCountTextView.setText(Integer.toString(listNum));
+
+                            }
+                        }
+
+                    }
+                    //바뀐 상태를 변경해줌
+                    vAdapter.notifyDataSetChanged();
+                }
+
+                if (listSizeCheck==deleteListSize){
+                    Toast.makeText(VoteMain.this, "선택된 항목이 없습니다.", Toast.LENGTH_LONG).show();
+                }
+                else
+                    Toast.makeText(VoteMain.this, "삭제 완료", Toast.LENGTH_LONG).show();
 
             }
         });
